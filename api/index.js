0c7b131c370adea14b8971af2150563f95dbbafb
ANS-375-trinhdtk <trinhdtk@ans-net.co.jp>
2025-01-15 17:37:33 +0900

    implement GitHub signature verification in Zoom controller

--- api/index.js
+++ api/index.js
@@ -1,35 +1,15 @@
 const express = require('express');
-const axios = require('axios');
+const routes = require('../src/routes/v1');
+
+
 const app = express();
-const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.post('/e9800998ecf8427e/demo', async (req, res) => {
-    try {
-        const requestData = req.body;
+// v1 api routes
+app.use('/v1', routes);
 
-        // Call another API
-        const response = await axios.post('https://integrations.zoom.us/chat/webhooks/incomingwebhook/z2luu-DaRAyinHSwdVd0Mw', requestData, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': '1AExRFXVQD-x6NjHVSuc1A'
-            }
-        });
-
-        res.status(response.status).send(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('An error occurred');
-    }
-});
-
-app.get("/", (req, res) => res.send("Congratulation 🎉🎉! Our Express server is Running on Vercel"));
-
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+app.get("/", (req, res) => res.send("Congratulation 🎉🎉! Our Express server is Running on Vercel with restruct"));
 
 module.exports = app;
