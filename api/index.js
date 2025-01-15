a114d296c8e41e1cbe773140976ad32fdd7eb381
ANS-375-trinhdtk <trinhdtk@ans-net.co.jp>
2025-01-15 10:59:38 +0900

    add demo endpoint for Zoom webhook integration and update server configuration

--- api/index.js
+++ api/index.js
@@ -1,14 +1,35 @@
 const express = require('express');
-const routes = require('../src/routes/v1');
-
+const axios = require('axios');
 const app = express();
+const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// v1 api routes
-app.use('/v1', routes);
+app.post('/e9800998ecf8427e/demo', async (req, res) => {
+    try {
+        const requestData = req.body;
 
-app.get("/", (req, res) => res.send("Congratulation 🎉🎉! Our Express server is Running on Vercel with restruct"));
+        // Call another API
+        const response = await axios.post('https://integrations.zoom.us/chat/webhooks/incomingwebhook/z2luu-DaRAyinHSwdVd0Mw', requestData, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': '1AExRFXVQD-x6NjHVSuc1A'
+            }
+        });
+
+        res.status(response.status).send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred');
+    }
+});
+
+app.get("/", (req, res) => res.send("Congratulation 🎉🎉! Our Express server is Running on Vercel"));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
 
 module.exports = app;
